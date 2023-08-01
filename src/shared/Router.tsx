926c212708f2308639ea2a1138b7d655dee0838a
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-02 00:21:05 +0900

    :sparkles: 투두 리스트에서 제목 클릭하면 상세 페이지로 이동

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -4,6 +4,8 @@ import RootLayout from "components/layout/RootLayout";
 import Intro from "components/pages/Intro";
 import MainLayout from "components/layout/MainLayout";
 import Main from "components/pages/Main";
+import Detail from "components/pages/Detail";
+import NotFound from "components/pages/NotFound";
 
 const Router = createBrowserRouter([
   {
@@ -21,10 +23,20 @@ const Router = createBrowserRouter([
           {
             path: "",
             element: <Main />,
+          },
+        ]
+      },
+      {
+        path: "/detail/:id",
+        element: <MainLayout />,
+        children: [
+          {
+            path: "",
+            element: <Detail />
           }
         ]
       },
-    ]
+    ],
   }
 ])
 
